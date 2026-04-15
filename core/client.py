576bb0f7e5038256fd63e0b80d16fee909c4b9ae
joseph2202HF <zafimamyjeanjoseph@gmail.com>
2026-04-15 12:41:31 +0300

    Amélioration du client : ajout d'entrée utilisateur et gestion d'erreurs

--- core/client.py
+++ core/client.py
@@ -1,19 +1,22 @@
 import socket
 from utils.config import HOST, PORT
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
 
-filename = "test.txt"
+    filename = input("Entrez le nom du fichier à envoyer : ")
 
-file = open(filename, "rb")
-data = file.read(1024)
-
-while data:
-    client.send(data)
+    file = open(filename, "rb")
     data = file.read(1024)
 
-file.close()
-client.close()
+    while data:
+        client.send(data)
+        data = file.read(1024)
 
-print("Fichier envoyé ✔️")
+    file.close()
+    client.close()
+
+    print("Fichier envoyé ✔️")
+except Exception as e:
+    print(f"Erreur : {e}")
